9f8a6b744cd749a5d504862faf30023209065beb
nataly <nataly.pescador@gmail.com>
2024-03-19 18:54:02 -0500

    Solucionando errores de token

--- src/main/java/com/proyectoGanApp/GanApp/GanAppApplication.java
+++ src/main/java/com/proyectoGanApp/GanApp/GanAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GanAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GanAppApplication.class, args);
 	}
-
 }
