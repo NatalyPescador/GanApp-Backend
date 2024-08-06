7596d8c0a28786a391a5b01bf5899863f16e6cbc
Nataly Guzman Pescador <132100591+NatalyPescador@users.noreply.github.com>
2024-08-05 20:27:05 -0500

    Logica culminada para traer productos publicados

--- src/main/java/com/proyectoGanApp/GanApp/repository/ProductoRepository.java
+++ src/main/java/com/proyectoGanApp/GanApp/repository/ProductoRepository.java
@@ -2,9 +2,15 @@ package com.proyectoGanApp.GanApp.repository;
 
 import com.proyectoGanApp.GanApp.model.ProductoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<ProductoEntity, Long>{
+
+    @Query("SELECT p FROM ProductoEntity p WHERE p.usuarioId = :userId")
+    List<ProductoEntity> getProductsByUserId(Long userId);
 
 }
