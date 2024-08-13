9d7d92ecf74e5762affe8f7c55b859ec877f33db
JohhanPC <johanlpc05@gmail.com>
2024-08-13 11:45:50 -0500

    Cambios review entity

--- src/main/java/com/proyectoGanApp/GanApp/model/ReviewEntity.java
+++ src/main/java/com/proyectoGanApp/GanApp/model/ReviewEntity.java
@@ -1,10 +1,7 @@
 package com.proyectoGanApp.GanApp.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
