f23e260b7ef668729ac234f79d0d85b90231f273
JohhanPC <johanlpc05@gmail.com>
2024-09-10 07:56:12 -0500

    Creacion de configuracion de cors

--- src/main/java/com/proyectoGanApp/GanApp/GanAppApplication.java
+++ src/main/java/com/proyectoGanApp/GanApp/GanAppApplication.java
@@ -3,7 +3,6 @@ package com.proyectoGanApp.GanApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class GanAppApplication {
 	public static void main(String[] args) {
