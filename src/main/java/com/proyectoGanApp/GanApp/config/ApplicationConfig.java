ae1ff9df16ff3707da240b8808a8bbed20942675
JohhanPC <johanlpc05@gmail.com>
2024-09-11 19:29:40 -0500

    creacion rama para despliegue

--- src/main/java/com/proyectoGanApp/GanApp/config/ApplicationConfig.java
+++ src/main/java/com/proyectoGanApp/GanApp/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.proyectoGanApp.GanApp.config;
 
 import com.proyectoGanApp.GanApp.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
